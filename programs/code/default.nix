742e838054b4c0ec912189e54e7eb4dde3eee947
ntrinite <trinitenoah@gmail.com>
2024-11-26 20:17:50 -0500

    python packages works

--- programs/code/default.nix
+++ programs/code/default.nix
@@ -1,15 +1,10 @@
 # Can import other nix modules here
 # Other modules can just import this folder and it will use the default.nix if another file isn't specified
-{ pkgs, lib, ... }: {
-  imports = [
-    ./vscode.nix
-    # ./python.nix
-  ];
+{ pkgs, lib, ... }:
+{
+  imports = [ ./vscode.nix ];
 
   home.packages = [
-#    (import ./python.nix {
- #     inherit pkgs;
-  #    inherit lib;
-   # })
+    (import ./python.nix { inherit pkgs; })
   ];
 }
--- programs/code/python.nix
+++ programs/code/python.nix
@@ -1,63 +1,53 @@
 # TODO: change this to an overlay structure and handle the error collisons
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 let
   python = pkgs.python311;
+  python_pkgs = pkgs.python311Packages;
 
-  # dted = python.pkgs.buildPythonPackage rec {
-  #   pname = "dted";
-  #   version = "1.1.0";
-  #   src = python_pkgs.fetchPypi {
-  #     inherit pname version;
-  #     sha256 = "sha256-Sa9MY03ku4r6z/us9eJNBCD7UU/6W82Oqv6BqfY1O/k=";
-  #   };
-  # };
 
-  # grpcio-reflection = python.pkgs.buildPythonPackage rec {
-  #   pname = "grpcio-reflection";
-  #   version = python_pkgs.grpcio.version;
-  #   src = python_pkgs.fetchPypi {
-  #     inherit pname version;
-  #     sha256 = "sha256-iSXeqHqESALgRjhJgJ1j3Q/KLFj+wPZIilljaDT1WkY=";
-  #   };
-  #   propagatedBuildInputs = [ python python_pkgs.grpcio ];
-  #   doCheck = false;
+  dted = python_pkgs.buildPythonPackage rec {
+    pname = "dted";
+    version = "1.1.0";
+    src = python_pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-Sa9MY03ku4r6z/us9eJNBCD7UU/6W82Oqv6BqfY1O/k=";
+    };
+  };
 
-  # };
+  grpc-requests = python_pkgs.buildPythonPackage rec {
+    pname = "grpc_requests";
+    version = "0.1.20";
+    src = python_pkgs.fetchPypi {
+      inherit pname version;
+      sha256 =
+      "sha256-p2dE14HFp786vejcd40kxOF96znRc9hQlv88ruPRUlA=";
+    };
+    patchPhase = "touch requirements.txt ";
+    propagatedBuildInputs =
+      [ python_pkgs.grpcio python_pkgs.grpcio-reflection ];
+    doCheck = false;
+  };
 
-  # grpc-requests = python.pkgs.buildPythonPackage rec {
-  #   pname = "grpc_requests";
-  #   version = "0.1.10";
-  #   src = python_pkgs.fetchPypi {
-  #     inherit pname version;
-  #     sha256 =
-  #     "sha256-+eIWpGGRZhxHitcrqnl855wrHciKD7MQTDPgBUAmLbA=";
-  #   };
-  #   patchPhase = "touch requirements.txt ";
-  #   propagatedBuildInputs =
-  #     [ pkgs.python3Packages.grpcio python.pkgs.python3Packages.grpcio-reflection ];
-  #   doCheck = false;
-  # };
-
-in python.withPackages (ppkgs:
-  with ppkgs; [
+in
+python.withPackages (
+  ppkgs: with ppkgs; [
     numpy
     scipy
     pandas
     matplotlib
-    pysimplegui
     pyside6
     grpcio
     grpcio-tools
     grpcio-reflection
-    # grpc-requests
+    grpc-requests
     pylint # linting
 
     # ML libraries/utilities
-    # opencv4
-    # tensorflow
-    # pytorch
-    # scikit-learn
+    opencv4
+    tensorflow-bin
+    pytorch
+    scikit-learn
 
     # Needed for robot tests
     icmplib
@@ -71,5 +61,6 @@ in python.withPackages (ppkgs:
 
     # Random library for really only one script I've ever used
     haversine
-    dted #FIX: isn't being recognized?
-  ])
+    dted
+  ]
+)
